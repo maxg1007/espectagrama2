e98a1e96434663ad526451fe15cba2ca287fad5d
maxg1007 <90800419+maxg1007@users.noreply.github.com>
2022-12-27 16:16:40 -0300

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+
+export default class CreatePost extends Component {
+  render() {
+    return <Text>Tela de criar</Text>;
+  }
+}
